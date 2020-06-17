f232e0bdd582988fc44dd20c1c88350828541ac5
minfrin <minfrin@users.noreply.github.com>
2020-06-18 01:37:58 +0200

    Add support for the current-user-principal.

--- mod_dav_access.c
+++ mod_dav_access.c
@@ -22,6 +22,10 @@
  *
  */
 #include <apr_lib.h>
+#include <apr_escape.h>
+#include <apr_strings.h>
+
+#include "ap_expr.h"
 
 #include "httpd.h"
 #include "http_config.h"
@@ -34,6 +38,12 @@
 #include "mod_dav.h"
 
 module AP_MODULE_DECLARE_DATA dav_access_module;
+
+typedef struct
+{
+    int principal_url_set :1;
+    ap_expr_info_t *principal_url;
+} dav_calendar_config_rec;
 
 /* forward-declare the hook structures */
 static const dav_hooks_liveprop dav_hooks_liveprop_access;
@@ -160,12 +170,142 @@ static const dav_liveprop_group dav_access_liveprop_group =
     &dav_hooks_liveprop_access
 };
 
-
-typedef struct
+static const char *dav_access_principal(request_rec *r)
 {
-    int dav_access_set :1;
-    int dav_access;
-} dav_calendar_config_rec;
+    dav_calendar_config_rec *conf = ap_get_module_config(r->per_dir_config,
+                                                &dav_access_module);
+
+    if (r->user && conf->principal_url) {
+        const char *err = NULL, *prefix, *suffix;
+
+    	prefix = ap_expr_str_exec(r, conf->principal_url, &err);
+        if (err) {
+            ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r,
+                            "Failure while evaluating the principal URL expression for '%s', "
+                            "no principal URL returned: %s", r->uri, err);
+            return NULL;
+        }
+
+        suffix = apr_pescape_path_segment(r->pool, r->user);
+
+        return apr_pstrcat(r->pool, prefix, "/", suffix, NULL);
+    }
+
+    return NULL;
+}
+
+static const char *dav_access_resource_principal(const dav_resource *resource)
+{
+    request_rec *r = resource->hooks->get_request_rec(resource);
+
+    return dav_access_principal(r);
+}
+
+static dav_prop_insert dav_access_insert_prop(const dav_resource *resource,
+		int propid, dav_prop_insert what, apr_text_header *phdr)
+{
+    const char *value;
+    const char *s;
+    apr_pool_t *p = resource->pool;
+    const dav_liveprop_spec *info;
+    int global_ns;
+
+    switch (propid) {
+    case DAV_ACCESS_PROPID_current_user_principal:
+
+        value = dav_access_resource_principal(resource);
+        if (value)
+            value = apr_psprintf(p, "<D:href>%s</D:href>", value);
+        else
+            value = "<D:unauthenticated/>";
+        break;
+
+    default:
+        /* ### what the heck was this property? */
+        return DAV_PROP_INSERT_NOTDEF;
+    }
+
+    /* assert: value != NULL */
+
+    /* get the information and global NS index for the property */
+    global_ns = dav_get_liveprop_info(propid, &dav_access_liveprop_group, &info);
+
+    /* assert: info != NULL && info->name != NULL */
+
+    if (what == DAV_PROP_INSERT_VALUE) {
+        s = apr_psprintf(p, "<lp%d:%s>%s</lp%d:%s>" DEBUG_CR,
+                         global_ns, info->name, value, global_ns, info->name);
+    }
+    else if (what == DAV_PROP_INSERT_NAME) {
+        s = apr_psprintf(p, "<lp%d:%s/>" DEBUG_CR, global_ns, info->name);
+    }
+    else {
+        /* assert: what == DAV_PROP_INSERT_SUPPORTED */
+        s = apr_pstrcat(p,
+                        "<D:supported-live-property D:name=\"",
+                        info->name,
+                        "\" D:namespace=\"",
+                        dav_access_namespace_uris[info->ns],
+                        "\"/>" DEBUG_CR, NULL);
+    }
+    apr_text_append(p, phdr, s);
+
+    /* we inserted what was asked for */
+    return what;
+}
+
+static int dav_access_is_writable(const dav_resource *resource, int propid)
+{
+    const dav_liveprop_spec *info;
+
+    (void) dav_get_liveprop_info(propid, &dav_access_liveprop_group, &info);
+    return info->is_writable;
+}
+
+static dav_error *dav_access_patch_validate(const dav_resource *resource,
+    const apr_xml_elem *elem, int operation, void **context, int *defer_to_dead)
+{
+    /* We have no writable properties */
+    return NULL;
+}
+
+static dav_error *dav_access_patch_exec(const dav_resource *resource,
+    const apr_xml_elem *elem, int operation, void *context,
+    dav_liveprop_rollback **rollback_ctx)
+{
+    /* We have no writable properties */
+    return NULL;
+}
+
+static void dav_access_patch_commit(const dav_resource *resource, int operation,
+    void *context, dav_liveprop_rollback *rollback_ctx)
+{
+    /* We have no writable properties */
+}
+
+static dav_error *dav_access_patch_rollback(const dav_resource *resource,
+    int operation, void *context, dav_liveprop_rollback *rollback_ctx)
+{
+    /* We have no writable properties */
+    return NULL;
+}
+
+static const dav_hooks_liveprop dav_hooks_liveprop_access =
+{
+    dav_access_insert_prop,
+    dav_access_is_writable,
+    dav_access_namespace_uris,
+    dav_access_patch_validate,
+    dav_access_patch_exec,
+    dav_access_patch_commit,
+    dav_access_patch_rollback
+};
+
+static int dav_access_find_liveprop(const dav_resource *resource,
+    const char *ns_uri, const char *name, const dav_hooks_liveprop **hooks)
+{
+    return dav_do_find_liveprop(ns_uri, name, &dav_access_liveprop_group, hooks);
+}
 
 static dav_error *dav_access_options_header(request_rec *r,
 		const dav_resource *resource, apr_text_header *phdr)
@@ -205,27 +345,35 @@ static void *merge_dav_access_dir_config(apr_pool_t *p, void *basev, void *addv)
 	dav_calendar_config_rec *add = (dav_calendar_config_rec *) addv;
 	dav_calendar_config_rec *base = (dav_calendar_config_rec *) basev;
 
-    new->dav_access = (add->dav_access_set == 0) ? base->dav_access : add->dav_access;
-    new->dav_access_set = add->dav_access_set || base->dav_access_set;
+    new->principal_url = (add->principal_url_set == 0) ? base->principal_url : add->principal_url;
+    new->principal_url_set = add->principal_url_set || base->principal_url_set;
 
     return new;
 }
 
-static const char *set_dav_access(cmd_parms *cmd, void *dconf, int flag)
+static const char *set_dav_principal_url(cmd_parms *cmd, void *dconf, const char *url)
 {
     dav_calendar_config_rec *conf = dconf;
+    const char *expr_err = NULL;
 
-    conf->dav_access = flag;
-    conf->dav_access_set = 1;
+    conf->principal_url = ap_expr_parse_cmd(cmd, url, AP_EXPR_FLAG_STRING_RESULT,
+            &expr_err, NULL);
+
+    if (expr_err) {
+        return apr_pstrcat(cmd->temp_pool,
+                "Cannot parse expression '", url, "': ",
+                expr_err, NULL);
+    }
+
+    conf->principal_url_set = 1;
 
     return NULL;
 }
 
 static const command_rec dav_access_cmds[] =
 {
-    AP_INIT_FLAG("DavAccess",
-        set_dav_access, NULL, RSRC_CONF | ACCESS_CONF,
-        "When enabled, the URL space will support calendars."),
+    AP_INIT_TAKE1("DavAccessPrincipalUrl", set_dav_principal_url, NULL, RSRC_CONF | ACCESS_CONF,
+        "Set the URL template to use for the principal URL."),
     { NULL }
 };
 
@@ -246,6 +394,9 @@ static void register_hooks(apr_pool_t *p)
     dav_register_liveprop_group(p, &dav_access_liveprop_group);
 
     dav_options_provider_register(p, "dav_access", &options);
+
+    dav_hook_find_liveprop(dav_access_find_liveprop, NULL, NULL, APR_HOOK_MIDDLE);
+
 }
 
 AP_DECLARE_MODULE(dav_access) =
